a43110235ca77eae590a82a0594d3fd0177691d8
Pasit Sri-intarasut <ong.pasit@gmail.com>
2022-03-31 22:47:59 +0700

    feat: add temperature

--- pages/api/weather.js
+++ pages/api/weather.js
@@ -1,7 +1,13 @@
 const { AirThai } = require("air-thai-api")
+var weather = require('weather-js');
 
 
 export default async function handler(req, res){
-    const result = await AirThai({ lat: 13.670809600000002, long: 100.6501888 })
-    res.json(result)
+    const data = await AirThai({ lat: 13.670809600000002, long: 100.6501888 })
+//     weather.find({search: 'Bankok', degreeType: 'C'}, function(err, result) {
+//   if(err) console.log(err);
+ 
+//   res.json(JSON.stringify(result, null, 2));
+// });
+    res.json(data)
 }
