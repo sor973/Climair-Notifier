a4d2b1b90b0892471046e78fa38200b98dd4640a
Pasit Sri-intarasut <ong.pasit@gmail.com>
2022-04-20 00:36:08 +0700

    feat: update line id to mongodb

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -9,13 +9,55 @@ export default async (req, res) => {
     res.status(200)
 }
 
+async function getUser(){
+    const MongoURL = 'mongodb://cpre_ubicom:7M72J9M3RFx29s75@192.168.42.201:27017/?authSource=cpreauth&readPreference=primary&appname=MongoDB+Compass&directConnection=true&ssl=false'
+    const MongoOption = {
+        tlsAllowInvalidCertificates: true,
+        useUnifiedTopology: true
+    };
+    const cli = new Mongocli(MongoURL,MongoOption);
+    await cli.connect()
+    let users = []
+    await cli.db('ubicom').collection('userdatabase').find({}).forEach((doc)=>{
+        users.push(doc)
+    });
+    return users
+}
+
+async function updateLineId(reply_token, token){
+    const MongoURL = 'mongodb://cpre_ubicom:7M72J9M3RFx29s75@192.168.42.201:27017/?authSource=cpreauth&readPreference=primary&appname=MongoDB+Compass&directConnection=true&ssl=false'
+    const MongoOption = {
+        tlsAllowInvalidCertificates: true,
+        useUnifiedTopology: true
+    };
+    const cli = new Mongocli(MongoURL,MongoOption);
+    await cli.connect()
+    let users = []
+    await cli.db('ubicom').collection('userdatabase').updateOne(
+        {
+            token:token
+        },
+        {
+            $set:{lineId: reply_token}
+        }
+    )
+}
+
 async function reply(reply_token, msg) {
     let TOKEN = "WwdL+wbqpyoZ6WPXFrA10+koTCU7pkin+35Mg+UoghguM9AG4Kv6/PzVRz7oUTxGQPeD2qt5jyFpzcWiemeRUGlM2gFtlcBhPZSxtGH7yjx9Grc/CxeVq1Gh4r+VeLateDsWRUzLaz55vKCqpLY7MQdB04t89/1O/w1cDnyilFU="
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${TOKEN}`
     }
+    let users = await getUser();
     let textReply = 'มีอะไรให้ช่วยคะ';
+    for (const user of users){
+        if(msg === user.token){
+            await updateLineId(reply_token, token)
+            textReply = "update lineid ของคุณเรียบร้อยค่ะ"
+            break;
+        }
+    }
     if (msg === "pm2.5"){
         const data = await getAq()
         textReply = data['pm25']
