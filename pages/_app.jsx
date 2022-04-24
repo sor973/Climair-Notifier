a60bb905788a6f767ba828bf389e597683565fb0
Sitthikorn Aimkhon <basza5589@gmail.com>
2022-04-24 17:27:18 +0700

    config fontawesome

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,8 @@
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
